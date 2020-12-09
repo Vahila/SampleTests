6764775c0752d68db117f0a1ed260256691046c7
Vahila Kayithi <vkayithi@mathworks.com>
2020-12-09 10:57:49 +0530

    Test cases

--- test/sqTest/testSq.m
+++ test/sqTest/testSq.m
@@ -1,4 +1,4 @@
-classdef testCalc < matlab.unittest.TestCase
+classdef (TestTags = {'Square'}) testSq < matlab.unittest.TestCase
     %TESTCALC Summary of this class goes here
     %   Detailed explanation goes here
     
@@ -8,12 +8,5 @@ classdef testCalc < matlab.unittest.TestCase
             %   Detailed explanation goes here
             testCase.verifyEqual(sqNum(2), 4);
         end
-        
-        function testMultiplication(testCase)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            testCase.verifyEqual(mulNum(2,2), 4);
-        end
     end
 end
-
