6a920af4934f06dfa3a4b69f62153a6b2667f177
Vahila <70003902+Vahila@users.noreply.github.com>
2020-12-10 09:42:38 +0530

    Update testMul.m

--- test/mulTest/testMul.m
+++ test/mulTest/testMul.m
@@ -1,4 +1,4 @@
-classdef (TestTags = {'Multiply'}) testMul < matlab.unittest.TestCase
+classdef (TestTags = {'Square'}) testMul < matlab.unittest.TestCase
     %TESTCALC Summary of this class goes here
     %   Detailed explanation goes here
     
