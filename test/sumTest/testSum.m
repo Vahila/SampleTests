9acf0e8c39dbfac71c4984a350bd231e784bed2d
Vahila Kayithi <vkayithi@mathworks.com>
2020-12-11 09:22:34 +0530

    Test cases for test folder

---
+++ test/sumTest/testSum.m
@@ -0,0 +1,13 @@
+classdef testSum < matlab.unittest.TestCase
+    %TESTCALC Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    methods(Test)        
+        function testMultiplication(testCase)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            a = 2+2;
+            testCase.verifyEqual(a, 4);
+        end
+    end
+end
